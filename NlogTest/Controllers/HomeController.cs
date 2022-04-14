0c656bfc06ec773f3475b56372e67b67a8437a87
Johnny <johnnychen@changingtec.com>
2022-04-14 10:57:09 +0800

    [add] init proj

---
+++ NlogTest/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NlogTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NlogTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _logger.LogWarning(1, "inject into HomeController");
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation(55,"Hello, this is the index!");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
